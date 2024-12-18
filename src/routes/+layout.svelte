3e425c2c6934da4801aad40ca868b4ef8f3eaad9
⛐ <26d0@tuta.io>
2024-12-18 13:15:35 +0900

    modified:   src/routes/+layout.svelte 	new file:   static/images/ogp.png

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,14 @@
+<svelte:head>
+    <title>うさささ</title>
+    <meta property="og:title" content="うさささ" />
+    <meta property="og:description" content="うさささ" />
+    <meta property="og:image" content="https://usasaki-app.pages.dev/images/ogp.png" />
+    <meta property="og:url" content="https://usasaki-app.pages.dev/" />
+    <meta property="og:type" content="website" />
+    <!-- Twitter用 -->
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
