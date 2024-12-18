0c9bf1e0c1941e68a2c6ee85714dbd57b4aa88d0
⛐ <26d0@tuta.io>
2024-12-18 13:19:29 +0900

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <svelte:head>
     <title>うさささ</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="android-chrome" sizes="192x192" href="/android-chrome-192x192.png">
+    <link rel="android-chrome" sizes="512x512" href="/android-chrome-512x512.png">
     <meta property="og:title" content="うさささ" />
     <meta property="og:description" content="うさささ" />
     <meta property="og:image" content="https://usasaki-app.pages.dev/images/ogp.png" />
